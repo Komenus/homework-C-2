16f362e5dab09f8900d4d9743cc144d3cb183096
dmitriyzheleznyak <komenus@gmail.com>
2022-08-10 15:14:24 +0300

    Task1 done

---
+++ Desktop/homework2/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзачное число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA >= 100 && numberA < 1000 ) 
+{
+    numberA = numberA /10;
+    Console.WriteLine("Вторая цифра числа " + " " + numberA % 10 );
+}
+else
+{
+    Console.WriteLine("Вы ввели некорректное число ");
+}
