6a8374cc529a04f7ab19a9c3e3a92ab9fcec717c
dmitriyzheleznyak <komenus@gmail.com>
2022-08-10 15:03:22 +0300

    Task2 done

---
+++ Desktop/homework2/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA < 100)
+{
+     Console.WriteLine("Третьей цифры нет ");
+}
+else while(numberA >= 1000)
+{
+    numberA = numberA / 10;
+}
+if (numberA > 100)
+{
+numberA = numberA - numberA / 100 * 100;
+numberA = numberA - numberA / 10 * 10;
+Console.WriteLine("Третья цфира " + " " + numberA);
+}
