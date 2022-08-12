093aca497a675576eab720561d69fe36dcee1828
dmitriyzheleznyak <komenus@gmail.com>
2022-08-12 14:45:41 +0300

    Task 3 done

---
+++ Desktop/homework2/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите порядковый номер дня недели ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if(numberA == 6 || numberA == 7)
+{
+    Console.WriteLine("Этот порядковый номер соотвествует выходному дню ");
+}
+else if(numberA >7)
+{
+    Console.WriteLine("В неделе 7 дней !");
+}
+else 
+{
+    Console.WriteLine("Этот порядковый номер  не соотвествует выходному дню ");
+}
